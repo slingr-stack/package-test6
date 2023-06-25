c0ef8ecb1561beadc296a36b37f780ff72aa28ca
Franco Ojeda <fojeda@slingr.io>
2023-06-25 12:32:21 -0300

    init pkg

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# package-test6
+# Title 1
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+## Title 2
+* Bullet points 1
+* Bullet points 2
+* Bullet points 3
+* Bullet points 4
+
+## Title 2
+Link test: https://www.slingr.io/
+
+### Title 3
+*this is cursive* **this is bold**
---
+++ scripts/lib.js
@@ -0,0 +1,49 @@
+exports.setMessage = function (msg) {
+    return config.set("message",msg);
+}
+
+exports.getMessage = function () {
+    return config.get("message");
+}
+
+exports.serviceTest = function (){
+    var val = config.get("message");
+    var response = svc.http.get({
+        url: "https://postman-echo.com/get",
+        params: {
+            queryVal: val
+        },
+        headers: {
+            key: "123"
+        },
+        forceDownload: false,
+        downloadSync: false,
+        fileName: "",
+        fullResponse: false,
+        connectionTimeout: "5000",
+        readTimeout: "60000",
+        defaultCallback: false
+    });
+    return JSON.stringify(response);
+}
+
+exports.serviceTest2 = function (){
+    var val = config.get("message");
+    var response = dependencies.http.get({
+        url: "https://postman-echo.com/get",
+        params: {
+            queryVal: val
+        },
+        headers: {
+            key: "123"
+        },
+        forceDownload: false,
+        downloadSync: false,
+        fileName: "",
+        fullResponse: false,
+        connectionTimeout: "5000",
+        readTimeout: "60000",
+        defaultCallback: false
+    });
+    return JSON.stringify(response);
+}
